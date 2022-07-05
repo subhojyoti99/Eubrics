c2390fa4ec80bfb6ae9cd1f5a89389a2037ca02a
SUBHOJYOTI SINGHA <84560030+subhojyoti99@users.noreply.github.com>
2022-07-05 14:08:03 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -11,7 +11,7 @@ export default function navbar() {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             
-            <Link to="/" type="submit" className="navbar-brand fw-bolder fs-4 mx-auto text-dark">AssignmenT</Link>
+            <Link to="/" type="submit" className="navbar-brand fw-bolder fs-4 mx-auto text-dark">AssignmenT_ClickMe</Link>
             <Link to="/assignment/login" className="btn btn-outline-dark ms-auto px-4 rounded-pill btn-sm bg-dark text-white-50">
                 <i className="fa fa-sign-in me-2"></i>Login</Link>
             <Link to="/assignment/register" className="btn btn-outline-dark ms-2 px-4 rounded-pill bg-dark text-white-50">
